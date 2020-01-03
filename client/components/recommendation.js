e8501b766441ca3a9fe46850dad65ce24f8ee507
Daniel Avellar <danielavellar15@hotmail.com>
2020-01-03 01:58:02 -0300

    pegando os produtos do banco

--- client/components/recommendation.js
+++ client/components/recommendation.js
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Components } from "/imports/plugins/core/components/lib";
+//import { Components } from "/imports/plugins/core/components/lib";
+import CatalogGrid from "@reactioncommerce/components/CatalogGrid/v1";
+import { Components } from "@reactioncommerce/reaction-components";
+import { Products } from "/lib/collections";
+import { i18next } from "/client/api";
 
 
-class Recommendation extends React.Component {
+
+
+class Recommendation extends Component {  
+  
   static propTypes = {
     product: PropTypes.object
   };
 
+  getProducts() {
+    const products = Products.find({ type: 'simple' }, { limit: 2 }).fetch();
+    console.log(products);
+    return products;
+  }
+  
+
   render() {
+     
+    const { currencyCode } = "USD";
+    const  products   = this.getProducts();
+    const badgeLabels = {
+      BACKORDER: i18next.t("productDetail.backOrder", "Backorder"),
+      LOW_QUANTITY: i18next.t("productDetail.limitedSupply", "Limited Supply"),
+      SOLD_OUT: i18next.t("productDetail.soldOut", "Sold Out!")
+    };
+    
 
     return (
       <div>
         <div>
           <h3>Recomendacoes</h3>
         </div>
-        <Components.Products></Components.Products>
+        {/* <CatalogGrid 
+          currencyCode={currencyCode}
+          products={products}
+          badgeLabels={badgeLabels}        
+        onItemClick={(event, product) => {
+          event.preventDefault();
+          console.log(product.title);
+        }} /> */}
+        <Components.Products></Components.Products>  
       </div>
     );
   }
